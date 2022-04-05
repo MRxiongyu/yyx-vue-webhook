7ae4fbdfece11977f2f7d675b687d5606cab12af
Yu.Xiong <247770359.com>
2022-04-05 21:28:11 +0800

    init

--- vue-back.sh
+++ vue-back.sh
@@ -6,8 +6,8 @@ git pull origin master
 echo "开始构建容器"
 docker build -t yyx-vue-back:0.0.1 .
 echo "删除旧容器"
-docker stop yyx-vue-back:0.0.1
-docker rm yyx-vue-back:0.0.1
+docker stop yyx-vue-back-container
+docker rm yyx-vue-back-container
 echo '启动新容器'
 docker container run -p 3000:3000 --name yyx-vue-back-container -d yyx-vue-back:0.0.1
 
